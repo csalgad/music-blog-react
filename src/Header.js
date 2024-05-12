43235ddc81a1661726ae1826d2c729b28b94993f
Chris Salgado <chrisasalgado@gmail.com>
2024-05-11 23:39:40 -0700

    init

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+
+function Header(){
+    return (
+        <>
+            <div class="title">
+                <h1>Hause Special &reg;</h1>
+                <h2>Music Blog</h2>
+            </div>                
+        </>
+    )
+}
+
+export default Header;
